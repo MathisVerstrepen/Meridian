a61d4563215577f3cd1ae1f279dc511ecfcc8752
Mathis Verstrepen <mathisverstrepen@gmail.com>
2025-03-30 19:53:30 -0400

    Update prompt block UI

--- ui/components/ui/graph/node/prompt.vue
+++ ui/components/ui/graph/node/prompt.vue
@@ -1,11 +1,11 @@
 <script lang="ts" setup>
-import type { NodeProps } from "@vue-flow/core";
-import { Position, Handle } from "@vue-flow/core";
-import { NodeResizer } from "@vue-flow/node-resizer";
+import type { NodeProps } from '@vue-flow/core';
+import { Position, Handle } from '@vue-flow/core';
+import { NodeResizer } from '@vue-flow/node-resizer';
 
 const props = defineProps<NodeProps>();
 
-const emit = defineEmits(["updateNodeInternals"]);
+const emit = defineEmits(['updateNodeInternals']);
 </script>
 
 <template>
@@ -17,25 +17,32 @@ const emit = defineEmits(["updateNodeInternals"]);
     ></NodeResizer>
 
     <div
-        class="bg-blue-200 text-black rounded-lg p-4 shadow-lg border-2 border-blue-300 w-full h-full flex flex-col"
+        class="bg-slate-blue text-black rounded-xl p-4 pt-3 shadow-lg border-2 border-slate-blue-dark w-full h-full flex flex-col transition-all duration-200 ease-in-out"
+        :class="{ 'opacity-50': props.dragging }"
     >
-        <p class="text-sm text-gray-500">{{ props.id }}</p>
-        <label
-            for="prompt-textarea"
-            class="block text-sm font-medium text-gray-700 mb-2 flex-shrink-0 w-fit"
-        >
-            Prompt
+        <!-- <p class="text-sm text-obsidian">{{ props.dragging }}</p> -->
+        <label for="prompt-textarea" class="mb-3 w-fit flex items-center gap-2">
+            <Icon
+                name="iconoir:input-field"
+                style="color: var(--color-soft-silk); height: 1.5rem; width: 1.5rem"
+                class="opacity-80"
+            />
+            <span class="text-soft-silk/80 font-bold -translate-y-[1px]">Prompt</span>
         </label>
         <textarea
             type="text"
             id="prompt-textarea"
             v-model="props.data.prompt"
-            class="nodrag w-full flex-1 min-h-0 rounded-lg focus:outline-none focus:ring-0 resize-none border-2 p-2 border-blue-300/50"
+            class="nodrag w-full rounded-lg focus:outline-none focus:ring-0 resize-none p-2 bg-soft-silk/50 h-full"
             placeholder="Enter your prompt here"
         ></textarea>
     </div>
 
-    <Handle type="source" :position="Position.Bottom" style="background: dodgerblue;" />
+    <Handle
+        type="source"
+        :position="Position.Bottom"
+        style="background: var(--color-slate-blue-dark)"
+    />
 </template>
 
 <style scoped></style>
