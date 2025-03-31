d69834788d84f14cac2f83e09c28f6440fa9017d
Mathis Verstrepen <mathisverstrepen@gmail.com>
2025-03-31 16:33:54 -0400

    Add outfit font

--- ui/components/ui/sidebar-history.vue
+++ ui/components/ui/sidebar-history.vue
@@ -2,9 +2,13 @@
 
 <template>
     <div
-        class="bg-anthracite/75 border-stone-gray/10 absolute top-2 left-2 z-10 h-[calc(100%-1rem)] w-[25rem]
-            rounded-2xl border-2 shadow-lg backdrop-blur-md"
-    ></div>
+        class="bg-anthracite/75 border-stone-gray/10 absolute top-2 left-2 z-10 flex h-[calc(100%-1rem)] w-[25rem]
+            flex-col items-center justify-start rounded-2xl border-2 px-4 py-10 shadow-lg backdrop-blur-md"
+    >
+        <h1 class="mb-8 flex items-center space-x-2 text-stone-gray text-2xl font-bold font-outfit">
+            Meridian
+        </h1>
+    </div>
 </template>
 
 <style scoped></style>
