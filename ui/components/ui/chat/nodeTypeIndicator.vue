282ea7832216327c3852afe76e0e0d74a29b051a
Mathis Verstrepen <mathisverstrepen@gmail.com>
2025-05-30 11:53:58 -0400

    fix: add node name on indicator hover

--- ui/components/ui/chat/nodeTypeIndicator.vue
+++ ui/components/ui/chat/nodeTypeIndicator.vue
@@ -8,12 +8,14 @@ defineProps<{
 
 <template>
     <span
-        class="absolute top-0 right-0 h-4 w-8 rounded-tr-xl rounded-bl-lg"
+        class="hover:text-obsidian absolute top-0 right-0 h-4 w-8 cursor-default overflow-hidden rounded-tr-xl
+            rounded-bl-lg px-2 text-xs font-bold text-transparent hover:w-fit"
         :class="{
             'bg-terracotta-clay': nodeType === NodeTypeEnum.PARALLELIZATION,
             'bg-olive-grove': nodeType === NodeTypeEnum.TEXT_TO_TEXT,
         }"
     >
+        {{ nodeType === NodeTypeEnum.PARALLELIZATION ? 'Parallelization' : 'Text to Text' }}
     </span>
 </template>
 
