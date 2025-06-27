0d89ab60ac49a54d281737ab09c29fb4724d9448
Mathis Verstrepen <mathisverstrepen@gmail.com>
2025-06-26 22:47:31 -0400

    fix: enhance edge compatibility checks and update target handle identifiers

--- ui/composables/useEdgeCompatibility.ts
+++ ui/composables/useEdgeCompatibility.ts
@@ -2,11 +2,13 @@ import { type GraphNode, type Connection, type HandleConnectableFunc } from '@vu
 
 const acceptedMapping: Record<string, string[]> = {
     prompt: ['prompt'],
-    context: ['textToText'],
+    context: ['textToText', 'parallelization'],
     attachment: ['filePrompt'],
 };
 
 export const useEdgeCompatibility = () => {
+    const { warning } = useToast();
+
     /**
      * Checks if a connection between two nodes is compatible based on node types.
      *
@@ -24,18 +26,29 @@ export const useEdgeCompatibility = () => {
         getNodes: globalThis.ComputedRef<GraphNode<any, any, string>[]>,
     ): Boolean => {
         let sourceNode = getNodes.value.find((node) => node.id === connection.source);
-        let targetType = connection.targetHandle?.split('_')[0];
+        let targetType = connection.targetHandle?.split('_')[1];
 
         if (!sourceNode || !targetType) {
+            warning('Invalid connection: source node or target type is missing.');
             return false;
         }
 
-        return acceptedMapping[targetType]?.includes(sourceNode.type);
+        if (connection.targetHandle?.split('_')[2] === connection.sourceHandle?.split('_')[2]) {
+            warning('Invalid connection: source and target handles cannot be from the same node.');
+            return false;
+        }
+
+        if (acceptedMapping[targetType]?.includes(sourceNode.type)) {
+            return true;
+        }
+
+        warning('Invalid connection: incompatible node types.');
+        return false;
     };
 
     const handleConnectableInputContext: HandleConnectableFunc = (node, connectedEdges) => {
         for (const edge of connectedEdges) {
-            if (edge.targetHandle === 'context_' + node.id) {
+            if (edge.targetHandle === 'target_context_' + node.id) {
                 return false;
             }
         }
@@ -44,7 +57,7 @@ export const useEdgeCompatibility = () => {
 
     const handleConnectableInputPrompt: HandleConnectableFunc = (node, connectedEdges) => {
         for (const edge of connectedEdges) {
-            if (edge.targetHandle === 'prompt_' + node.id) {
+            if (edge.targetHandle === 'target_prompt_' + node.id) {
                 return false;
             }
         }
@@ -53,7 +66,7 @@ export const useEdgeCompatibility = () => {
 
     const handleConnectableInputAttachment: HandleConnectableFunc = (node, connectedEdges) => {
         for (const edge of connectedEdges) {
-            if (edge.targetHandle === 'attachment_' + node.id) {
+            if (edge.targetHandle === 'target_attachment_' + node.id) {
                 return false;
             }
         }
