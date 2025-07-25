c2f39898d194e9996581e03a9b733eeaf7154a44
Mathis Verstrepen <mathisverstrepen@gmail.com>
2025-07-25 18:37:27 -0400

    feat: add detail plan execution status section in execution plan component

--- ui/components/ui/graph/executionPlan.vue
+++ ui/components/ui/graph/executionPlan.vue
@@ -1,6 +1,12 @@
 <script lang="ts" setup>
 import type { ExecutionPlanResponse, ExecutionPlanStep } from '@/types/chat';
+import { NodeTypeEnum } from '@/types/enums';
+import { useVueFlow } from '@vue-flow/core';
 import { motion } from 'motion-v';
+
+const props = defineProps<{
+    graphId: string;
+}>();
 
 // --- Stores ---
 const canvasSaveStore = useCanvasSaveStore();
@@ -9,6 +15,7 @@ const canvasSaveStore = useCanvasSaveStore();
 const { saveGraph } = canvasSaveStore;
 
 // --- Composables ---
+const { fitView } = useVueFlow('main-graph-' + props.graphId);
 const graphEvents = useGraphEvents();
 const nodeRegistry = useNodeRegistry();
 const { error, warning } = useToast();
@@ -20,6 +27,13 @@ const currentStep = ref(0);
 const doneTable = ref<Record<string, number>>({});
 const isExecuting = ref(false);
 const pendingExecutions = ref<Set<string>>(new Set());
+const isOpen = ref(false);
+const startTime = ref(Date.now());
+const selectedCategories = ref<Record<string, boolean>>({
+    not_started: true,
+    in_progress: true,
+    done: true,
+});
 
 // --- Computed Properties ---
 const progressRatio = computed(() => {
@@ -70,6 +84,7 @@ const executer = async () => {
     currentStep.value = 0;
     doneTable.value = {};
     pendingExecutions.value.clear();
+    startTime.value = Date.now();
 
     try {
         // Initialize all steps as not started
@@ -128,6 +143,14 @@ const stopExecution = async () => {
     warning('Execution stopped', { title: 'Execution Stopped' });
 };
 
+const focusToNode = (nodeId: string) => {
+    fitView({
+        nodes: [nodeId],
+        duration: 1000,
+        padding: 2,
+    });
+};
+
 // --- Watchers ---
 watch(
     () => isExecuting.value,
@@ -165,55 +188,159 @@ onMounted(() => {
 <template>
     <AnimatePresence>
         <motion.div
-            v-if="plan"
+            v-if="plan && plan.steps.length > 0"
             key="execution-plan"
-            :initial="{ opacity: 0, y: -50, width: '10%' }"
+            :initial="{ opacity: 0, y: -50, width: '10%', height: '3rem' }"
             :animate="{
                 opacity: 1,
                 y: 0,
                 width: '25%',
+                height: isOpen ? '20rem' : '3rem',
                 transition: {
                     y: { duration: 0.15 },
                     width: { delay: 0.05, duration: 0.2 },
+                    height: { duration: 0.3, ease: 'easeInOut' },
                 },
             }"
             :exit="{
                 opacity: 0,
                 y: -50,
                 width: '10%',
+                height: '3rem',
                 transition: {
                     width: { duration: 0.2 },
                     y: { delay: 0.15, duration: 0.1 },
+                    height: { duration: 0.3, ease: 'easeInOut' },
                 },
             }"
             class="bg-anthracite/75 border-stone-gray/10 absolute top-2 left-1/2 z-10 flex h-12 w-[25%]
-                -translate-x-1/2 items-center justify-between gap-5 rounded-2xl border-2 p-1 px-2 shadow-lg
-                backdrop-blur-md"
+                -translate-x-1/2 flex-col rounded-2xl border-2 p-1 px-2 shadow-lg backdrop-blur-md"
         >
-            <div class="text-soft-silk bg-soft-silk/10 rounded-lg px-2 py-0.5 text-sm font-bold">
-                {{ currentStep }} / {{ plan?.steps.length }}
+            <div class="flex h-9 w-full shrink-0 items-center justify-between gap-5">
+                <div
+                    class="text-soft-silk bg-soft-silk/10 rounded-lg px-2 py-0.5 text-sm font-bold"
+                >
+                    {{ currentStep }} / {{ plan?.steps.length }}
+                </div>
+
+                <UiGraphProgressBar :value="progressRatio" />
+
+                <div>
+                    <button
+                        class="nodrag bg-soft-silk/10 hover:bg-soft-silk/20 dark:text-soft-silk text-anthracite relative flex h-8
+                            w-8 flex-shrink-0 cursor-pointer items-center justify-center rounded-2xl transition-all duration-200
+                            ease-in-out"
+                        @click="stopExecution"
+                        v-if="progressRatio < 1"
+                    >
+                        <UiIcon name="MaterialSymbolsStopRounded" class="h-5 w-5" />
+                    </button>
+
+                    <div
+                        class="nodrag bg-olive-grove/40 text-soft-silk relative flex h-8 w-8 flex-shrink-0 cursor-pointer
+                            items-center justify-center rounded-2xl"
+                        v-else
+                    >
+                        <UiIcon name="MaterialSymbolsCheckSmallRounded" class="h-5 w-5" />
+                    </div>
+                </div>
             </div>
 
-            <UiGraphProgressBar :value="progressRatio" />
+            <div class="my-2 flex items-center justify-between gap-2 px-2" v-if="isOpen">
+                <NuxtTime
+                    :datetime="startTime"
+                    relative
+                    class="text-soft-silk/50 text-xs font-bold"
+                />
 
-            <div>
-                <button
-                    class="nodrag bg-soft-silk/10 hover:bg-soft-silk/20 dark:text-soft-silk text-anthracite relative flex h-8
-                        w-8 flex-shrink-0 cursor-pointer items-center justify-center rounded-2xl transition-all duration-200
-                        ease-in-out"
-                    @click="stopExecution"
-                    v-if="progressRatio < 1"
-                >
-                    <UiIcon name="MaterialSymbolsStopRounded" class="h-5 w-5" />
-                </button>
-
-                <div
-                    class="nodrag bg-olive-grove/40 text-soft-silk relative flex h-8 w-8 flex-shrink-0 cursor-pointer
-                        items-center justify-center rounded-2xl"
-                    v-else
-                >
-                    <UiIcon name="MaterialSymbolsCheckSmallRounded" class="h-5 w-5" />
+                <div class="flex items-center gap-2">
+                    <button
+                        class="bg-stone-gray/20 border-stone-gray/50 h-4 w-8 flex-shrink-0 cursor-pointer items-center
+                            justify-center rounded-full border-2 transition-all duration-200 ease-in-out"
+                        @click="selectedCategories.not_started = !selectedCategories.not_started"
+                        :class="{
+                            '!bg-stone-gray !border-transparent': selectedCategories.not_started,
+                        }"
+                        title="Toggle Not Started"
+                    ></button>
+                    <button
+                        class="bg-slate-blue/20 border-slate-blue/50 h-4 w-8 flex-shrink-0 cursor-pointer items-center
+                            justify-center rounded-full border-2 transition-all duration-200 ease-in-out"
+                        @click="selectedCategories.in_progress = !selectedCategories.in_progress"
+                        :class="{
+                            '!bg-slate-blue !border-transparent': selectedCategories.in_progress,
+                        }"
+                        title="Toggle In Progress"
+                    ></button>
+                    <button
+                        class="bg-olive-grove/20 border-olive-grove/50 h-4 w-8 flex-shrink-0 cursor-pointer items-center
+                            justify-center rounded-full border-2 transition-all duration-200 ease-in-out"
+                        @click="selectedCategories.done = !selectedCategories.done"
+                        :class="{
+                            '!bg-olive-grove !border-transparent': selectedCategories.done,
+                        }"
+                        title="Toggle Done"
+                    ></button>
                 </div>
+            </div>
+
+            <ul
+                v-if="isOpen"
+                class="hide-scrollbar mb-2 flex h-min w-full flex-wrap items-center justify-center gap-2 overflow-y-auto"
+            >
+                <li
+                    v-for="step in plan?.steps"
+                    :key="step.node_id"
+                    class="bg-stone-gray/10 border-stone-gray/20 text-soft-silk relative flex h-10 w-[48%] items-center
+                        justify-between overflow-hidden rounded-lg border-2 px-2 py-2 transition-all duration-200
+                        ease-in-out"
+                    :class="{
+                        '!bg-olive-grove/20 !border-olive-grove/50': doneTable[step.node_id] === 2,
+                        '!bg-slate-blue/20 !border-slate-blue/50': doneTable[step.node_id] === 1,
+                    }"
+                    v-show="
+                        (selectedCategories.not_started && doneTable[step.node_id] === 0) ||
+                        (selectedCategories.in_progress && doneTable[step.node_id] === 1) ||
+                        (selectedCategories.done && doneTable[step.node_id] === 2)
+                    "
+                >
+                    <span
+                        class="hover:text-obsidian absolute top-0 left-0 h-2 w-8 rounded-tl-lg rounded-br-xl"
+                        :class="{
+                            'bg-terracotta-clay': step.node_type === NodeTypeEnum.PARALLELIZATION,
+                            'bg-olive-grove': step.node_type === NodeTypeEnum.TEXT_TO_TEXT,
+                            'bg-sunbaked-sand-dark': step.node_type === NodeTypeEnum.ROUTING,
+                        }"
+                    >
+                    </span>
+                    <span class="mx-2 text-[9px] font-bold"
+                        >{{ step.node_id.slice(0, 24) }}...</span
+                    >
+                    <button
+                        class="nodrag bg-stone-gray/10 hover:bg-stone-gray/20 dark:text-soft-silk text-anthracite relative flex h-6
+                            w-6 flex-shrink-0 cursor-pointer items-center justify-center rounded-2xl transition-all duration-200
+                            ease-in-out"
+                        @click="focusToNode(step.node_id)"
+                    >
+                        <UiIcon name="LetsIconsTarget" class="h-4 w-4" />
+                    </button>
+                </li>
+            </ul>
+
+            <div
+                class="bg-anthracite hover:bg-obsidian border-stone-gray/10 absolute -bottom-4 left-1/2 flex h-6 w-10
+                    -translate-x-1/2 cursor-pointer items-center justify-center rounded-lg border-2 transition
+                    duration-200 ease-in-out"
+                @click="isOpen = !isOpen"
+                role="button"
+            >
+                <UiIcon
+                    name="TablerChevronCompactLeft"
+                    class="text-stone-gray h-6 w-6 -rotate-90 transition-transform duration-200 ease-in-out"
+                    :class="{
+                        'rotate-90': isOpen,
+                    }"
+                />
             </div>
         </motion.div>
     </AnimatePresence>
