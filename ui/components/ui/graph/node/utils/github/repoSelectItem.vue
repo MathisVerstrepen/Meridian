ded8c758f0a3a318e5f68e0edf3a255b3f345f49
Mathis Verstrepen <mathisverstrepen@gmail.com>
2025-09-01 10:17:09 -0400

    fix: improve styling and layout of repository selection components

--- ui/components/ui/graph/node/utils/github/repoSelectItem.vue
+++ ui/components/ui/graph/node/utils/github/repoSelectItem.vue
@@ -7,23 +7,29 @@ defineProps<{
 </script>
 
 <template>
-    <div class="flex items-center justify-between w-full">
-        <div class="flex items-center justify-center gap-2">
-            <div class="bg-stone-gray/20 flex items-center rounded-full pr-2 pl-1">
-                <UiIcon name="MaterialSymbolsLightStarOutlineRounded" class="text-stone-gray h-5 w-5" />
+    <div class="flex grow items-center justify-between overflow-hidden">
+        <div class="flex min-w-0 flex-1 items-center gap-2 overflow-hidden">
+            <div class="bg-stone-gray/20 flex shrink-0 items-center rounded-full pr-2 pl-1">
+                <UiIcon
+                    name="MaterialSymbolsLightStarOutlineRounded"
+                    class="text-stone-gray h-5 w-5"
+                />
                 <span class="text-stone-gray/70 text-xs">{{ repo.stargazers_count }}</span>
             </div>
 
-            <span class="text-stone-gray/70 text-sm">{{ repo.full_name }}</span>
+            <p
+                class="text-stone-gray/70 min-w-0 overflow-hidden text-sm text-ellipsis whitespace-nowrap"
+            >
+                {{ repo.full_name }}
+            </p>
         </div>
 
         <span
             v-if="repo.private"
-            class="bg-golden-ochre/20 text-golden-ochre rounded-full px-2 py-0.5 text-xs"
+            class="bg-golden-ochre/20 text-golden-ochre shrink-0 rounded-full px-2 py-0.5 text-xs"
             >Private</span
-        >        
+        >
     </div>
-
 </template>
 
 <style scoped></style>
