2efbb42e9a29fa77a66e4a7f5d970ed7c7082987
Mathis Verstrepen <mathisverstrepen@gmail.com>
2025-09-15 09:48:03 -0400

    feat: add messageTeleport component for chat UI

---
+++ ui/components/ui/chat/utils/messageTeleport.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup></script>
+
+<template>
+    <div class="bg-obsidian/50 text-soft-silk/60 absolute bottom-20 flex flex-col rounded-xl p-1">
+        <button
+            class="hover:bg-soft-silk/5 rounded-[8px] bg-transparent p-1 duration-200 ease-in-out cursor-pointer"
+        >
+            <UiIcon name="LineMdChevronSmallUp" class="h-8 w-8" />
+        </button>
+        <button
+            class="hover:bg-soft-silk/5 rounded-[8px] bg-transparent p-1 duration-200 ease-in-out cursor-pointer"
+        >
+            <UiIcon name="LineMdChevronSmallUp" class="h-8 w-8 rotate-180" />
+        </button>
+    </div>
+</template>
+
+<style scoped></style>
