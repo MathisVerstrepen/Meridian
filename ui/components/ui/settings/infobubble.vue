ee231880ef71356c5fdecaba2d345628a36c24b3
Mathis Verstrepen <mathisverstrepen@gmail.com>
2025-09-19 12:16:30 -0400

    fix: refactor infobubble component to improve panel positioning and structure

--- ui/components/ui/settings/infobubble.vue
+++ ui/components/ui/settings/infobubble.vue
@@ -6,21 +6,21 @@ const props = defineProps<{
 }>();
 
 const buttonRef = ref<HTMLElement | null>(null);
-const panelRef = ref<HTMLElement | null>(null);
+const menuPosition = ref({ top: 0, left: 0 });
 
 const updatePanelPosition = () => {
-    if (props.direction === 'left' && buttonRef.value && panelRef.value) {
+    if (buttonRef.value) {
         const rect = buttonRef.value.getBoundingClientRect();
-        panelRef.value.style.left = `${rect.left - 500}px`;
-        panelRef.value.style.top = `${rect.top}px`;
+        menuPosition.value = {
+            top: rect.top + 32,
+            left: rect.left,
+        };
     }
 };
 
 watch(open, (newVal) => {
-    if (newVal && props.direction === 'left') {
-        nextTick(() => {
-            updatePanelPosition();
-        });
+    if (newVal) {
+        nextTick(updatePanelPosition);
     }
 });
 </script>
@@ -28,27 +28,42 @@ watch(open, (newVal) => {
 <template>
     <HeadlessPopover class="relative">
         <HeadlessPopoverButton
-            ref="buttonRef"
-            as="div"
+            as="template"
             class="flex h-6 w-6 cursor-pointer items-center justify-center"
             @mouseover="open = true"
             @mouseleave="open = false"
         >
-            <UiIcon name="MaterialSymbolsInfoRounded" class="text-stone-gray h-4 w-4" />
+            <button ref="buttonRef">
+                <UiIcon name="MaterialSymbolsInfoRounded" class="text-stone-gray h-4 w-4" />
+            </button>
         </HeadlessPopoverButton>
 
-        <!-- IMPORTANT: When above a context that already has a backdrop-blur, this component backdrop-blur will not work -->
-        <HeadlessPopoverPanel
-            v-if="open"
-            ref="panelRef"
-            static
-            class="bg-anthracite/75 text-stone-gray fixed z-30 flex w-[500px] flex-col items-start rounded-lg p-4
-                shadow-lg backdrop-blur"
-            :class="{
-                '-translate-x-[480px]': props.direction === 'right',
-            }"
-        >
-            <slot name="default"/>
-        </HeadlessPopoverPanel>
+        <Teleport to="body">
+            <Transition
+                enter-active-class="transition duration-150 ease-out"
+                enter-from-class="transform scale-95 opacity-0"
+                enter-to-class="transform scale-100 opacity-100"
+                leave-active-class="transition duration-100 ease-in"
+                leave-from-class="transform scale-100 opacity-100"
+                leave-to-class="transform scale-95 opacity-0"
+            >
+                <HeadlessPopoverPanel
+                    v-if="open"
+                    static
+                    class="bg-obsidian/50 text-stone-gray border-stone-gray/10 fixed z-30 flex w-[500px] flex-col items-start
+                        rounded-lg border p-4 shadow-lg backdrop-blur-xl"
+                    :class="{
+                        'origin-top-right -translate-x-[480px]': props.direction === 'right',
+                        'origin-top-left': props.direction === 'left',
+                    }"
+                    :style="{
+                        top: `${menuPosition.top}px`,
+                        left: `${menuPosition.left}px`,
+                    }"
+                >
+                    <slot name="default" />
+                </HeadlessPopoverPanel>
+            </Transition>
+        </Teleport>
     </HeadlessPopover>
 </template>
