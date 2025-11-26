65e1c70a03c23881b4619b776080a848a11e619f
Mathis Verstrepen <mathisverstrepen@gmail.com>
2025-11-25 19:13:28 -0500

    feat: add image generation tool display in selected tools component

--- ui/app/components/ui/graph/node/utils/selectedTools.vue
+++ ui/app/components/ui/graph/node/utils/selectedTools.vue
@@ -32,6 +32,17 @@ const props = defineProps<{
         >
             <UiIcon name="MdiLinkVariant" class="h-5 w-5" />
         </div>
+        <div
+            v-if="props.data.selectedTools?.includes(ToolEnum.IMAGE_GENERATION)"
+            class="flex -translate-y-0.5 items-center rounded-lg p-1"
+            :class="{
+                'bg-soft-silk/10 text-soft-silk/80': props.theme === 'dark',
+                'bg-obsidian/10 text-obsidian/80': props.theme === 'light',
+            }"
+            title="Image Generation Enabled"
+        >
+            <UiIcon name="MdiImageMultipleOutline" class="h-4.5 w-4.5" />
+        </div>
     </div>
 </template>
 
