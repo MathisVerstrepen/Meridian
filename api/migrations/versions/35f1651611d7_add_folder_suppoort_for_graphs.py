b674739d3736b7b5832c7f2ab3cecd10037c303f
Mathis Verstrepen <mathisverstrepen@gmail.com>
2025-11-28 09:43:18 -0500

    fix: linter

--- api/migrations/versions/35f1651611d7_add_folder_suppoort_for_graphs.py
+++ api/migrations/versions/35f1651611d7_add_folder_suppoort_for_graphs.py
@@ -6,10 +6,9 @@ Create Date: 2025-11-27 12:18:34.848009
 
 """
 
-from alembic import op
 import sqlalchemy as sa
 import sqlmodel
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "35f1651611d7"
