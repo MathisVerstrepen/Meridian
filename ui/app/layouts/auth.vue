f3c841d687cca0316c3a845de4f1bfe7317530ad
Mathis Verstrepen <mathisverstrepen@gmail.com>
2025-12-30 13:07:59 -0500

    feat: Add footer with GitHub and contact links to the authentication layout

--- ui/app/layouts/auth.vue
+++ ui/app/layouts/auth.vue
@@ -17,5 +17,24 @@
         </picture>
 
         <slot />
+
+        <!-- Footer -->
+        <div class="text-stone-gray/50 absolute bottom-8 flex items-center gap-4 text-sm">
+            <a
+                href="https://github.com/MathisVerstrepen/Meridian"
+                target="_blank"
+                rel="noopener noreferrer"
+                class="hover:text-soft-silk transition-colors"
+            >
+                GitHub
+            </a>
+            •
+            <a
+                href="mailto:contact@meridianchat.com"
+                class="hover:text-soft-silk transition-colors"
+            >
+                Contact
+            </a>
+        </div>
     </div>
 </template>
