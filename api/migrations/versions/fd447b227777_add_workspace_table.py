2ddeaaa40eebf019bf830b7d0b614199ed5a0f8d
Mathis Verstrepen <mathisverstrepen@gmail.com>
2026-01-03 10:08:48 -0500

    feat: add UUID extension creation in workspace migration

--- api/migrations/versions/fd447b227777_add_workspace_table.py
+++ api/migrations/versions/fd447b227777_add_workspace_table.py
@@ -20,6 +20,8 @@ depends_on = None
 
 
 def upgrade() -> None:
+    op.execute('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+
     # 1. Create workspaces table
     op.create_table(
         "workspaces",
